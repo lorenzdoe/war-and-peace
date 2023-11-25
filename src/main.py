44fd9646f5d9b541f63827ae93a5e5c308b0bb84
Lorenz Doellinger <if21b084@technikum-wien.at>
2023-11-25 20:41:19 +0100

    map reduce, monads

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from itertools import islice, groupby
+from itertools import groupby
 from enum import Enum
-from typing import List, Tuple, Iterable, Generator, Iterator, Dict
+from typing import List, Tuple, Generator, Iterator, Dict
 from functools import partial
 
 from Monad import Maybe
@@ -33,8 +33,8 @@ def read_wordlists(war_file: str, peace_file: str)-> Maybe:
     """
     produces a dictionary with key is a word and value is either WAR or PEACE
     """
-    war_keywords = read_keywords('in/war.in')
-    peace_keywords = read_keywords('in/peace.in')
+    war_keywords = read_keywords(war_file)
+    peace_keywords = read_keywords(peace_file)
     
     if war_keywords.is_error or peace_keywords.is_error:
         return Maybe(None, is_error=True)
@@ -48,9 +48,8 @@ class WarOrPeace(Enum):
     WAR = 0
     PEACE = 1
 
-### functions ###
 
-def split_into_chapters(book: List[str], keyword: str) -> List[List[str]]:
+def split_into_chapters(book: List[str], keyword: str) -> Tuple[Tuple[str]]:
     """
     Takes List[str] and splits it into chapters by keyword
     """
@@ -58,9 +57,9 @@ def split_into_chapters(book: List[str], keyword: str) -> List[List[str]]:
     # group words by chapters
     chapters: Generator[Iterator] = (grouper for key, grouper in groupby(book, key=is_chapter) if not key)
     # transform words to lowercase and remove non-alphabetic characters
-    only_words_chapters: Generator = (list(map(clean_word, chapter)) for chapter in chapters)
+    only_words_chapters: Generator = (tuple(map(clean_word, chapter)) for chapter in chapters)
     # skip text before chapter 1
-    return list(only_words_chapters)[1:]
+    return tuple(only_words_chapters)[1:]
 
 
 def transform_chapter(key_dict) -> Tuple[Tuple]:
@@ -100,30 +99,50 @@ def map_chapter(key_dict: Dict[str, Enum], chapter: List[str], chapter_nr: int, 
 # bind map_chapter to specific types
 map_chapter_WoP = partial(map_chapter, type1=WarOrPeace.WAR, type2=WarOrPeace.PEACE)
 
-def calc_score(indices: Tuple[int], nr: int, chap_len: int) -> Dict[int, float]:
+def calc_score(indices: Tuple[int], chap_len: int) -> float:
     """
-    shuffle step from map reduce, returns a dict with key is chapter nr. and value is score for that chapter
+    returns a tuple with chapter nr. and score for that chapter
     """
     av_space: float = calc_av_space(indices)
     space_score: float = (1 / av_space) if av_space is not None else 0
     density_score: float = len(indices) / chap_len
-    return {
-        nr: (space_weight * space_score + density_weight * density_score ) * 100
-    }
+    return (space_weight * space_score + density_weight * density_score ) * 100
+
+def shuffle_chapters(chapters: Tuple[Dict[Enum, Tuple[int]]], type) -> Tuple[float]:
+    """
+    shuffle step from map reduce, returns a list of tuples with scores for that chapter
+    """
+    return tuple(calc_score(chapter[type], chapter['len']) for chapter in chapters)
+
+
+def print_results(reduced_chapters: List[Enum]) -> None:
+    for i, chapter in enumerate(reduced_chapters):
+        print(f'Chapter {i+1}: {chapter.name.lower()}-related')
 
 
 if __name__ == '__main__':
 
-    wordlist: dict = read_wordlists('in/war.in', 'in/peace.in').value
-    war_and_peace: List[str] = read_and_split('in/war_and_peace.in').value
-    chapters: List[List[str]] = split_into_chapters(book=war_and_peace,keyword='CHAPTER')
-    transformed_chapters = [transform_chapter(wordlist)(chapter) for chapter in chapters]
-    mapped_chapters = [map_chapter_WoP(wordlist, chapter, i+1) for i, chapter in enumerate(chapters)]
-    
-    for map_chap in mapped_chapters:
-        war_score = calc_score(map_chap[WarOrPeace.WAR], map_chap['nr'], map_chap['len'])[map_chap['nr']]
-        peace_score = calc_score(map_chap[WarOrPeace.PEACE], map_chap['nr'], map_chap['len'])[map_chap['nr']]
-        res = 'peace-related' if war_score < peace_score else 'war-related'
-        print(f"Chapter {map_chap['nr']}: {res}")
+    maybe_wordlist: Maybe = read_wordlists('in/war.in', 'in/peace.in')
+    maybe_war_and_peace: Maybe = read_and_split('in/war_and_peace.in')
 
+    if maybe_wordlist.is_error or maybe_war_and_peace.is_error:
+        print('Error reading files')
+        exit(1)
+
+    wordlist: dict = maybe_wordlist.value
+    war_and_peace: Tuple[str] = maybe_war_and_peace.value
+
+    chapters: Tuple[List[str]] = split_into_chapters(book=war_and_peace,keyword='CHAPTER')
+    transformed_chapters = tuple(transform_chapter(wordlist)(chapter) for chapter in chapters)
+
+    # map
+    mapped_chapters: Dict = tuple(map_chapter_WoP(wordlist, chapter, i+1) for i, chapter in enumerate(chapters))
+
+    # "shuffle"
+    war_scores: Tuple[float] = shuffle_chapters(mapped_chapters, WarOrPeace.WAR)
+    peace_scores: Tuple[float] = shuffle_chapters(mapped_chapters, WarOrPeace.PEACE)
+
+    # reduce
+    reduced: Tuple[Enum] = tuple(WarOrPeace.WAR if war_score > peace_score else WarOrPeace.PEACE for war_score, peace_score in zip(war_scores, peace_scores))
     
+    print_results(reduced)
